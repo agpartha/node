6a98598430c2c37dcc6040e185cd03aac5014cce
agpartha <code.ag@outlook.com>
2019-04-20 11:19:26 -0700

    Add request source and hostname to figure out which container is responding to the request

--- simpleserver/simpleserver.js
+++ simpleserver/simpleserver.js
@@ -1,9 +1,10 @@
-var http = require('http');
+const http = require('http');
+const os = require('os');
 
 var handleRequest = function(request, response) {
-  console.log('Received request for URL: ' + request.url);
+  console.log('Received request for URL: ' + request.url + ', from ' + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end('Hello World!');
-};
+  response.end('Hello ' + request.connection.remoteAddress + ', I am a hello server(' + os.hostname() + ')\n');
+}
 var www = http.createServer(handleRequest);
 www.listen(8080);
